9149a3ef23162debfdfcbbac9e894ea4b3466b33
savyasachibhat <159227457+savyasachibhat@users.noreply.github.com>
2025-02-12 21:33:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: './', // Ensures relative paths
+  server: {
+    historyApiFallback: true, // Fixes 404 on direct navigation
+  },
 })
