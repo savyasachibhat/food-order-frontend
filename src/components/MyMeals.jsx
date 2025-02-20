106aaf8d72c7e0f7c2afd331084ddd1c819a764d
savyasachi bhat <savyasachibhat52@gmail.com>
2025-02-20 12:53:24 +0530

    new

--- src/components/MyMeals.jsx
+++ src/components/MyMeals.jsx
@@ -1,21 +1,27 @@
 import { useContext } from "react";
 import UserProgressContext from "../store/UserProgressContext";
+import { Link } from "react-router-dom";
 
 const MyMeals = () => {
   const { user, userMeals } = useContext(UserProgressContext);
 
   return (
-    <div>
-      <h2>{user ? `${user.name}'s Ordered Meals` : "My Meals"}</h2>
-      {userMeals.length === 0 ? <p>No meals found.</p> : (
-        <ul>
-          {userMeals.map((meal, index) => (
-            <li key={index}>
-              <strong>{meal.name}</strong> - {meal.quantity} x ₹{meal.price}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="thanks-page-wrapper">
+      <div className="thanks-page">
+        <h1>{user ? `${user.name}'s Ordered Meals` : "My Meals"}</h1>
+        {userMeals.length === 0 ? (
+          <p>No meals found.</p>
+        ) : (
+          <ul>
+            {userMeals.map((meal, index) => (
+              <li key={index}>
+                <strong>{meal.name}</strong> - {meal.quantity} x ₹{meal.price}
+              </li>
+            ))}
+          </ul>
+        )}
+        <Link to="/">Home</Link>
+      </div>
     </div>
   );
 };
