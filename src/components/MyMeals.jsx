7f4d43537e766f5863285e2878767256feb4be89
savyasachi bhat <savyasachibhat52@gmail.com>
2025-02-20 14:24:32 +0530

    new

--- src/components/MyMeals.jsx
+++ src/components/MyMeals.jsx
@@ -1,9 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import UserProgressContext from "../store/UserProgressContext";
 import { Link } from "react-router-dom";
 
 const MyMeals = () => {
-  const { user, userMeals } = useContext(UserProgressContext);
+  const { user, userMeals, setUserMeals } = useContext(UserProgressContext);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    if (user) {
+      const fetchMeals = async () => {
+        try {
+          const response = await fetch(`${backendUrl}/user/meals`, {
+            method: "GET",
+            headers: { 
+              "Content-Type": "application/json",
+              name: user.name,
+              email: user.email
+            },
+          });
+          if (response.ok) {
+            const data = await response.json();
+            setUserMeals(data.meals);
+          } else {
+            // If no meals found, set empty array
+            setUserMeals([]);
+          }
+        } catch (error) {
+          console.error("Error fetching meals:", error);
+        }
+      };
+      fetchMeals();
+    }
+  }, [user, backendUrl, setUserMeals]);
 
   return (
     <div className="thanks-page-wrapper">
