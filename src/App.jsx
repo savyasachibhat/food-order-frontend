c5bd315f148cb232d1bb61267257017146dfb15a
savyasachi bhat <savyasachibhat52@gmail.com>
2025-02-20 12:03:04 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import MainPage from "./components/MainPage";
+// import Admin from "./components/Admin";
+// import ThanksPage from "./components/ThanksPage";
+// import { CartContextProvider } from "./store/CartContext";
+// import {UserProgressContextProvider} from "./store/UserProgressContext";
+// import MyMeals from "./components/MyMeals";
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <MainPage /> },
+//   { path: "/admin", element: <Admin /> },
+//   { path: "/thankyou", element: <ThanksPage /> },
+//   { path: "/mymeals", element: <MyMeals /> },
+// ]);
+// const App = () => {
+//   return (
+//     <div>
+//       <UserProgressContextProvider>
+//         <CartContextProvider>
+//           <RouterProvider router={router} />
+//         </CartContextProvider>
+//       </UserProgressContextProvider>
+//     </div>
+//   );
+// };
+
+// export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Admin from "./components/Admin";
 import ThanksPage from "./components/ThanksPage";
+import MyMeals from "./components/MyMeals";
 import { CartContextProvider } from "./store/CartContext";
-import {UserProgressContextProvider} from "./store/UserProgressContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
+import Header from "./components/Header";
 
-const router = createBrowserRouter([
-  { path: "/", element: <MainPage /> },
-  { path: "/admin", element: <Admin /> },
-  { path: "/thankyou", element: <ThanksPage /> },
-]);
 const App = () => {
   return (
-    <div>
-      <UserProgressContextProvider>
-        <CartContextProvider>
-          <RouterProvider router={router} />
-        </CartContextProvider>
-      </UserProgressContextProvider>
-    </div>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Router>  {/* Replace RouterProvider with BrowserRouter */}
+          <Header /> {/* Ensure header is inside Router so navigation works */}
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/thankyou" element={<ThanksPage />} />
+            <Route path="/mymeals" element={<MyMeals />} />
+          </Routes>
+        </Router>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 };
 
